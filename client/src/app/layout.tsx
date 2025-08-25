825f49baef2a908a1cdc6f798dfe58a87c453aa6
torikkyun <nduc42176@gmail.com>
2025-08-25 22:08:35 +0700

    Kết nối ví metamask

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Wagmi from '@/components/wagmi-provider';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,11 +25,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Wagmi>{children}</Wagmi>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Wagmi>{children}</Wagmi>
+        </ThemeProvider>
       </body>
     </html>
   );
