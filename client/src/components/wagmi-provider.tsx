13fc412fa4e1c82d1dc10dc41f785d2454b854b3
torikkyun <nduc42176@gmail.com>
2025-08-25 21:11:23 +0700

    Update database, cập nhật wagmi provider

--- client/src/components/wagmi-provider.tsx
+++ client/src/components/wagmi-provider.tsx
@@ -1,8 +1,15 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { config } from '@/lib/wagmi-config';
 
+const queryClient = new QueryClient();
+
 export default function Wagmi({ children }: { children: React.ReactNode }) {
-  return <WagmiProvider config={config}>{children}</WagmiProvider>;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
 }
